4625d1eb47f72c68b97a6041cc976fd23a9d7393
Zatoufly <76479469+zatoufly@users.noreply.github.com>
2022-10-11 22:00:32 +0200

    script deploiment windows

---
+++ prep-pc-zatou.ps1
@@ -0,0 +1,12 @@
+$MACHINE_NAME = Read-Host "Set PC Name"
+$MACHINE_DESC_NAME = Read-Host "Set PC Desc"
+
+# Désactiver l'UAC
+Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0
+
+# Renommer la machine
+Rename-Computer -NewName $MACHINE_NAME
+
+# Set une desc au PC
+$MACHINE_DESC_NAME2 = @{Description = $MACHINE_DESC_NAME}
+Set-CimInstance -Query 'Select * From Win32_OperatingSystem' -Property $MACHINE_DESC_NAME2
