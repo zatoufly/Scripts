5fa64bd9362701a0053012b03bc040283817546c
Zatoufly <76479469+zatoufly@users.noreply.github.com>
2022-10-17 20:56:22 +0200

    enable windows feature: windows sandbox

--- prep-pc-zatou.ps1
+++ prep-pc-zatou.ps1
@@ -10,3 +10,6 @@ Rename-Computer -NewName $MACHINE_NAME
 # Set une desc au PC
 $MACHINE_DESC_NAME2 = @{Description = $MACHINE_DESC_NAME}
 Set-CimInstance -Query 'Select * From Win32_OperatingSystem' -Property $MACHINE_DESC_NAME2
+
+# Install Windows Feature : Windows Sandbox
+Enable-WindowsOptionalFeature -FeatureName "Containers-DisposableClientVM" -Online -NoRestart -ErrorAction Stop
